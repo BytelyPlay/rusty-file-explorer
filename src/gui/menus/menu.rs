992b3993c5c207f3e679fdcd3286a32d53f6d5bd
BytelyPlay <universalnebula2024@outlook.com>
2026-08-30 13:40:51 +0200

    We're back to square one. I changed my mind and I am now going to use slint

--- src/gui/menus/menu.rs
+++
@@ -1,6 +0,0 @@
-use iced::Element;
-use crate::utils::message::Message;
-
-pub trait Menu {
-    fn create_menu(&self) -> Element<Message>;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,2 @@
-use crate::gui::context::Context;
-use crate::gui::menus::files_menu::FilesMenu;
-
-pub mod utils;
-pub mod gui;
-
-pub fn main() -> iced::Result {
-    iced::application(|| {
-        Context::new(
-            Box::new(
-                FilesMenu::new()
-            )
-        )
-    }, Context::update, Context::view)
-        .run()
+fn main() {
 }
